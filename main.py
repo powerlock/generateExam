9dce16dc69a24f196552cee402611d3a611a6674
powerlock <michael.guo002@gmail.com>
2023-04-24 12:07:23 -0700

    update regex to extract correct context for questions and options:

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 from docx import Document
 import re
 # Open the Word document
-doc = Document('chapter9.docx')
+doc = Document('test.docx')
 
 # Define the bank of questions with their levels of difficulty
 question_bank = {
@@ -25,17 +25,18 @@ for paragraph in doc.paragraphs:
     # Check if the paragraph starts with a number followed by a dot, indicating a new question
     if text and text[0].isdigit() and ')' in text:
         # Extract the question from the text
-        question = text.split(')')[1].strip()
+        question = re.sub(r'^\d+\)', '', text).strip()#text.split(')')[1].strip()
         current_question = question
-    elif any(option in text for option in ['A)', 'B)', 'C)', 'D)']):
+    # Check if the paragraph starts with a A, B, C or D followed by a half parenthesis, indicating a new option
+    elif text and text[0].isalpha() and ')' in text:
+    #elif any(option in text for option in ['A)', 'B)', 'C)', 'D)']):
         
-        # Find all the options for the text until the text contains the word 'Answer'
-        #options = re.findall(r'\b[A-D]\)\s+[\d.]+\s+\w+', text, re.DOTALL)
-        options = re.findall(r'\b[A-Z]\)\s+[\(\)\w\d]+', text, re.DOTALL)
+        # find the options in the text
+        options = re.sub(r'^\d+\)', '', text).strip()#text.split(')')[1].strip()
         # add the options to the list
         opt.append(options)
-         
-
+        print(opt)
+        
     #Check if the paragraph contains the answer
     elif 'Answer:' in text:
         match = re.search(r'Answer:\s*([A-D])', text)
@@ -53,11 +54,11 @@ for paragraph in doc.paragraphs:
         current_level = 'G'+str(level)
         # Add the current question to the question bank with the current level
         if current_question is not None and current_level is not None:
-            print(sum(opt, []))
+            
                     # Add the current question to the question bank with the options, answer and current level
             question_bank[current_level].append({
             "question": current_question,
-            "options": sum(opt, []),
+            "options": opt,
             
         })
             current_question = None
@@ -67,9 +68,9 @@ for paragraph in doc.paragraphs:
         
 
 # Input the number of questions for each level of difficulty
-num_g1 = int(input("Enter the number of G1 questions: "))
-num_g2 = int(input("Enter the number of G2 questions: "))
-num_g3 = int(input("Enter the number of G3 questions: "))
+# num_g1 = int(input("Enter the number of G1 questions: "))
+# num_g2 = int(input("Enter the number of G2 questions: "))
+# num_g3 = int(input("Enter the number of G3 questions: "))
 num_g4 = int(input("Enter the number of G4 questions: "))
 
 # Initialize the list to store the selected questions
@@ -77,22 +78,25 @@ selected_questions = []
 
 # Randomly select questions from the question bank based on the specified number of questions for each level of difficulty
 for level, questions in question_bank.items():
-    if level == "G1":
-        selected_questions.extend(random.sample(questions, num_g1))
-    elif level == "G2":
-        selected_questions.extend(random.sample(questions, num_g2))
-    elif level == "G3":
-        selected_questions.extend(random.sample(questions, num_g3))
-    elif level == "G4":
+    # if level == "G1":
+    #     selected_questions.extend(random.sample(questions, num_g1))
+    # elif level == "G2":
+    #     selected_questions.extend(random.sample(questions, num_g2))
+    # elif level == "G3":
+    #     selected_questions.extend(random.sample(questions, num_g3))
+    if level == "G4":
         selected_questions.extend(random.sample(questions, num_g4))
 
 # Create a new Word document to store the selected questions
 output_doc = Document()
-
 # Write the selected questions to the new Word document
-for i, question in enumerate(selected_questions, 1):
+for i, Question in enumerate(selected_questions, 1):
     # Add a new paragraph for each question
-    output_doc.add_paragraph(f"Question {i}: {question}")
+    question_text = Question['question']
+    options = Question['options']
+    output_doc.add_paragraph(f"Question {i}: {question_text}")
+    for option in options:
+        output_doc.add_paragraph(f"{option}")
 
 # Save the new Word document
 output_doc.save('generated_exam.docx')
